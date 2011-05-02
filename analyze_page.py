dfc012f5d2ad5fa263c3d7d15d45351438d35d16
Joseph Henrich <crimsonknave@gmail.com>
2011-05-02 13:49:53 -0400

    front end now calls the backend and colors only the significant words

--- analyze_page.py
+++ analyze_page.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python2
 
 import cherrypy
+import urllib
+import json
 
 class AnalyzeBox:
-  def __init__(self, data):
-    self.data = data
+  def __init__(self):
+    #self.data = data
+    self.path = 'http://localhost:8081'
     self.colors = {"neg":"FF0000", "pos":"00FF00", "unknown":"000000"}
+    self.weights = {}
 
   def index(self, text=""):
     text.strip()
+    weight_request = urllib.urlopen("{}/weights/?words={}".format(self.path, urllib.quote_plus(text)))
+    self.weights = json.loads(weight_request.next())
+    weight_request.close()
+    classification = urllib.urlopen("{}/classify/?words={}".format(self.path, urllib.quote_plus(text)))
+    self.classification = json.loads(classification.next())
+    classification.close()
+    print self.classification
     return """
 <form name="input" action="index" method="get">
 Text: <textarea id="source" name="text" rows="10" cols="50">
@@ -21,7 +32,7 @@ This is what you submitted:
 <br/>
 Key:
 <br/>
-{key}""".format(text=text, colored_text=self.color(text.split()), key="\n".join(["<font color='{}'>{}</font><br/>".format(value,key) for key, value in self.colors.items()]), classification=self.data.classify(word_feats(text.split())))
+{key}""".format(text=text, colored_text=self.color(text.split()), key="\n".join(["<font color='{}'>{}</font><br/>".format(value,key) for key, value in self.colors.items()]), classification=self.classification)
 
   index.exposed = True
 
@@ -35,9 +46,12 @@ Key:
   def color(self, words):
     output = ""
     for word in words:
-      label, weight = self.data.weight(word)
+      label, weight = self.weights[word]
       #print "{}: weight {}, label {}".format(word, weight, label)
-      output = output + " <font color='{}'>{}({:.2f})</font>".format(self.colors[label], word, weight)
+      color = "000000"
+      if (weight > self.weights['largest weights'][label]/5):
+        color = self.colors[label]
+      output = output + " <font color='{}'>{}({:.2f})</font>".format(color, word, weight)
     return output
 
   def print_classifiers(self, classifiers):
@@ -47,9 +61,5 @@ Key:
 def word_feats(words):
   return dict([(word, True) for word in words])
 
-import data_server
-
-stuff = data_server.Data()
-stuff.train()
-cherrypy.quickstart(AnalyzeBox(stuff))
+cherrypy.quickstart(AnalyzeBox())
 
